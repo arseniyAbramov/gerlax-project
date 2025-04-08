f8bd1673e1b50b9c0564c99176ee2100dc17d240
arseniyAbramov <arseniy2281337214@gmail.com>
2025-04-09 00:01:26 +0300

    Добавлена регистрация, логин и авторизация через Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\GameController;
 use App\Http\Controllers\Api\GenreController;
-// use App\Http\Controllers\Api\GameController;
+use App\Http\Controllers\Api\AuthController;
+
+
 
 Route::get('/message', function () {
     return response()->json([
@@ -13,6 +15,15 @@ Route::get('/message', function () {
 });
 Route::get('/games', [GameController::class, 'index']);       // список игр
 Route::get('/games/{id}', [GameController::class, 'show']);   // детальная страница игры
-Route::get('/genres', [GenreController::class, 'index']);  
 
+Route::get('/genres', [GenreController::class, 'index']);  
 Route::get('/genres/{slug}/games', [GenreController::class, 'gamesByGenre']);
+
+Route::post('/register', [AuthController::class, 'register'] );
+Route::post('/login', [AuthController::class, 'login'] );
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user',   [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
